ff1f1106d2df7db7faf329254355552434820940
Piyush Tanti <magicianworld99@gmail.com>
2026-06-21 22:20:17 +0530

    note16 Complex number summation

--- note15.cpp
+++ note15.cpp
@@ -12,7 +12,7 @@ class Employee{
 
     public:
         void setData(void){
-            cout<<"Enter the id "<<endl;
+            cout<<"Enter the id for"<<endl;
             cin>>id;
             count++;
         }
@@ -20,6 +20,12 @@ class Employee{
         void getData(void){
         cout << "The id of this employee is " << id << " and this is employee number " << count << endl;
         }
+
+    static void getCount(void){
+        // cout<<id; // throws an error.
+        // We can only use those variable which is static.
+        cout<<"The value of count is "<<count<<endl;
+    }
 };
 
 int Employee::count;
@@ -29,7 +35,11 @@ int main(){
 
     Alex.setData();
     Alex.getData();
+    Alex.getCount();
+
     Deon.setData();
     Deon.getData();
+    Deon.getCount();
+
     return 0;
 }
---
+++ note16.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+class complex{
+    int a, b;
+    public:
+        void setData(int real_no, int imaginary_no){
+            a = real_no;
+            b = imaginary_no;
+
+        }
+
+        void complexSum(complex cn1, complex cn2){
+            a = cn1.a + cn2.a;
+            b = cn1.b + cn2.b;
+
+        }
+
+        void printData(){
+            cout<<"The Complex number is "<<a<<"+"<<b<<"i"<<endl;
+        }
+};
+
+int main(){
+    complex c1, c2, c3;
+    c1.setData(5, 2);
+    c1.printData();
+
+    c2.setData(9, 6);
+    c2.printData();
+
+    c3.complexSum(c1, c2);
+    c3.printData();
+    return 0;
+}
